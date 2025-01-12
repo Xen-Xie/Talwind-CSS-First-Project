3b58894a51941d80446d386b6e9b36b61067444e
Hemel <rh189827@gmail.com>
2025-01-12 14:10:05 +0600

    solved

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { dirname, resolve } from "path";
+import { defineConfig } from "vite";
+
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                main: resolve(__dirname, "news.html"),
+            },
+        },
+    },
+});
